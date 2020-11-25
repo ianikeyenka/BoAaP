9dd0bd06b397f4ff132c358269fc79b5be17b1a0
IlyaAnik <71770143+IlyaAnik@users.noreply.github.com>
2020-11-25 23:59:49 +0300

    Update 4.1.c

--- ЛР№4/4.1.c
+++ ЛР№4/4.1.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+int main()
+{
+  int k,z,ch,r;
 
+  printf("Какая размерность будет в массиве?\n");
+  scanf("%d",&k);
+  int a[k];
+
+  for(int i = 0; i < k; i ++) {
+    printf("Введите элементы массива a[%d]=",i);
+    scanf("%d",&a[i]);
+  }
+  printf("Ваш массив: {");
+  for (int i = 0; i < k; i++) {
+    printf("%d", a[i]);
+    if (i < k-1) {
+      printf(", ");
+    } 
+  }
+  printf("}\n");
+
+  printf("На сколько нужно сдвинуть элементы массива влево?\n");
+  scanf("%d",&z);
+
+  for (int j = 0; j < z; j++) {
+    ch = a[0];
+    for (int i = 0; i < k-1 ; ++i) {
+      a[i] = a[i+1];
+    }
+    a[k-1] = ch;
+  }
+
+  printf("Массива после сдвига: {");
+  for (int i = 0; i < k; i++) {
+    printf("%d", a[i]);
+    if (i < k-1) {
+      printf(", ");
+    }
+  }
+  printf("}\n");
+
+  return 0;
+} 
